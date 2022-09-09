4f18954f90c1da061828a88d2fe7085bacab78dd
jona799t <jonathan.b.skettrup@gmail.com>
2022-09-09 14:22:09 +0200

    Funktioner returneres som json og man kan nu også få elever

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Python-Lectio
-En SDK til gymnasie siden Lectio
+En SDK til gymnasie siden Lectio  
+Dataen bliver returneret ud som JSON
 
 # Installation
 ```
@@ -14,13 +15,49 @@ import lectio
 
 client = lectio.sdk(brugernavn="mit brugernavn", adgangskode="min adgangskode", skoleId="mit skole id")
 ```
+## Elev ID
+```python
+import lectio
+
+elevId = client.elevId
+```
+
 ## Skema
 Se dit skema for en hvilken som helst uge. Hvis du ikke skriver uge/år på tager den for ugen næste skoledag
 ```python
-skemaUgeNæsteSkoledag = client.skema()
-skemaUgePåSpecifikUge = client.skema(uge=35, år=2022)
+skema = client.skema()
+skemaSpecifikUge = client.skema(uge=35, år=2022)
 ```
+Returneret format:
+```json
+[
+  {
+    "tidspunkt": "...",
+    "hold": "...",
+    "lærer": "...",
+    "andet": "...",
+    "absid": "..."
+  },
+  ...
+]
+```
+## Elever
+Få alle elever på skolen med et bestemt forbogstav.
+```python
+elever = client.elever(forbogstav="A")
+```
+Returneret format:
+```json
+[
+  {
+    "navn": "...",
+    "elevid": "..."
+  },
+  ...
+]
+```
+
 
 # To Do
    * Tilføj flere funktioner
-   * Måske gøre så hvad der bliver returneret er i json format i stedet for html
+   * Login med auto login key
---
+++ lectio/lectio.py
@@ -0,0 +1,173 @@
+import json
+import requests
+import re
+from bs4 import BeautifulSoup
+
+
+class sdk:
+    def __init__(self, brugernavn, adgangskode, skoleId):
+        self.brugernavn = brugernavn
+        self.adgangskode = adgangskode
+        self.skoleId = skoleId
+
+        self.session = requests.session()
+
+        self.login()
+
+    def login(self):
+        headers = {
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'accept-encoding': 'gzip, deflate, br',
+            'accept-language': 'en-DK,en;q=0.9',
+            'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="101", "Google Chrome";v="101"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Linux"',
+            'sec-fetch-dest': 'document',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-site': 'none',
+            'sec-fetch-user': '?1',
+            'upgrade-insecure-requests': '1',
+            'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.0.0 Safari/537.36',
+        }
+        resp = self.session.get(f"https://www.lectio.dk/lectio/{self.skoleId}/login.aspx", headers=headers)
+        soup = BeautifulSoup(resp.text, "html.parser")
+        payload = {}
+        for payloadItem in soup.find_all({"input": {"type": "hidden"}}):
+            payload[payloadItem.get('name')] = payloadItem.get('value')
+
+        del payload["query"]
+        del payload["m$defaultformbtn"]
+
+        payload["__EVENTTARGET"] = "m$Content$submitbtn2"
+        payload["m$Content$username"] = self.brugernavn
+        payload["m$Content$password"] = self.adgangskode
+        payload["m$Content$AutologinCbx"] = "on"
+        payload["LectioPostbackId"] = ""
+
+        headers = {
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'accept-encoding': 'gzip, deflate, br',
+            'accept-language': 'en-DK,en;q=0.9',
+            'cache-control': 'max-age=0',
+            'content-length': str(len(json.dumps(payload, separators=(',', ':')))),
+            'content-type': 'application/x-www-form-urlencoded',
+            'origin': 'https://www.lectio.dk',
+            'referer': f'https://www.lectio.dk/lectio/{self.skoleId}/login.aspx',
+            'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="101", "Google Chrome";v="101"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Linux"',
+            'sec-fetch-dest': 'document',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-site': 'same-origin',
+            'sec-fetch-user': '?1',
+            'upgrade-insecure-requests': '1',
+            'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.0.0 Safari/537.36',
+        }
+        resp = self.session.post(f"https://www.lectio.dk/lectio/{self.skoleId}/login.aspx", data=payload,
+                                 headers=headers)
+        soup = BeautifulSoup(resp.text, "html.parser")
+
+        successful = False
+        for meta in soup.find_all({"meta": {"name": "msapplication-starturl"}}):
+            if f"/lectio/{self.skoleId}/forside.aspx?" in str(meta.get("content")):
+                self.elevId = meta.get("content").split("?elevid=")[1]
+                successful = True
+                break
+        if not successful:
+            raise Exception("Kunne ikke finde elev id. Rapporter venligst dette på Github")
+
+    def skema(self, retry=False, uge=None, år=None, elevId=None):
+        if elevId == None:
+            elevId = self.elevId
+
+        if uge == None and år == None:
+            resp = self.session.get(
+                f"https://www.lectio.dk/lectio/{self.skoleId}/SkemaNy.aspx?type=elev&elevid={elevId}")
+        elif uge != None and år != None:
+            resp = self.session.get(
+                f"https://www.lectio.dk/lectio/{self.skoleId}/SkemaNy.aspx?type=elev&elevid={elevId}&week={uge}{år}")
+        else:
+            raise Exception("Enten skal hverken uge og år være i brug ellers skal både uge og år være i brug")
+
+        soup = BeautifulSoup(resp.text, "html.parser")
+
+        successful = False
+        skema = []
+        i = 0
+        for dag in soup.find_all("div", class_="s2skemabrikcontainer"):
+            if i != 0:
+                dag = BeautifulSoup(str(dag), "html.parser")
+                for modul in dag.find_all("a", class_="s2skemabrik"):
+                    modulDetailer = modul["data-additionalinfo"].split("\n\n")[0].split("\n")
+
+                    modulDict = {
+                        "tidspunkt": modulDetailer[-4],
+                        "hold": modulDetailer[-3].replace("Hold: ", ""),
+                        "lærer": modulDetailer[-2].replace("Lærere: ", "").replace("Lærer: ", ""),
+                        "lokale": modulDetailer[-1].replace("Lokale: ", "")
+                    }
+
+                    try:
+                        modulDict["andet"] = modul["data-additionalinfo"].split("\n\n")[1]
+                    except Exception:
+                        modulDict["andet"] = None
+                        pass
+
+                    modulDict["absid"] = re.search('absid=[0-9]+', modul["href"]).group().replace("absid=", "")
+
+                    skema.append(modulDict)
+            i += 1
+
+        if len(skema) > 0:
+            successful = True
+
+        if successful:
+            return skema
+        elif not retry:
+            self.login()
+            return self.skema(retry=True)
+        else:
+            return False
+
+    def elever(self, forbogstav, retry=False):
+        elever = []
+
+        # if forbogstav != None:
+        resp = self.session.get(f"https://www.lectio.dk/lectio/{self.skoleId}/FindSkema.aspx?type=elev&forbogstav={forbogstav}")
+        soup = BeautifulSoup(resp.text, "html.parser")
+        successful = False
+        for elev in soup.find_all("a"):
+            if "data-lectiocontextcard" in str(elev):
+                elever.append({"navn": elev.getText(), "elevid": elev["href"].split("elevid=")[1]})
+                successful = True
+
+        if successful:
+            return elever
+        elif not retry:
+            self.login()
+            return self.elever(retry=True)
+        else:
+            return False
+
+        # Ikke gør det neden under da man får ip ban fra lektio
+
+        # else:
+        #   resp = self.session.get(f"https://www.lectio.dk/lectio/681/FindSkema.aspx?type=elev")
+        #   soup = BeautifulSoup(resp.text, "html.parser")
+        #   forbogstaver = []
+        #   for forbogstav in soup.find_all("a"):
+        #       if ' href="/lectio/681/FindSkema.aspx?type=elev&amp;forbogstav=' in str(forbogstav):
+        #           forbogstaver.append(forbogstav.getText())
+
+        #   for elev in soup.find_all("a"):
+        #       if "data-lectiocontextcard" in str(elev):
+        #           print(elev.getText())
+        #           elever.append({"elevid": elev["href"].split("elevid=")[1], "navn": elev.getText()})
+
+        #   for forbogstav in forbogstaver:
+        #       resp = self.session.get(f"https://www.lectio.dk/lectio/681/FindSkema.aspx?type=elev&forbogstav={forbogstav}")
+        #       soup = BeautifulSoup(resp.text, "html.parser")
+        #       for elev in soup.find_all("a"):
+        #           if "data-lectiocontextcard" in str(elev):
+        #               print(elev.getText())
+        #               elever.append({"elevid": elev["href"].split("elevid=")[1], "navn": elev.getText()})
