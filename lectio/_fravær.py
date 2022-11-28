d80a838a4a5ca73ed7bb720637a423616175440e
jona799t <70392256+jona799t@users.noreply.github.com>
2022-11-28 17:19:55 +0100

    Nu med generalt fravær

--- lectio/_fravær.py
+++ lectio/_fravær.py
@@ -4,12 +4,16 @@ def fravær(self):
     resp = self.session.get(f"https://www.lectio.dk/lectio/{self.skoleId}/subnav/fravaerelev.aspx?elevid={self.elevId}")
     soup = BeautifulSoup(resp.text, "html.parser")
 
-    fravær = []
+    fravær = {
+        "generalt": [],
+        "moduler": "ikke mulig endnu da jeg har 0% fravær"
+    }
     for hold in soup.find("table", {"id": "s_m_Content_Content_SFTabStudentAbsenceDataTable"}).find_all("tr"):
         if hold.get("id") == None:
             row = hold.find_all("td")
-            fravær.append({
-                "hold": row[0],
-                "fravær_procent": row[1],
+            fravær["generalt"].append({
+                "hold": row[0].text,
+                "fravær_procent": row[1].text,
             })
-            print(fravær)
+
+    return fravær
