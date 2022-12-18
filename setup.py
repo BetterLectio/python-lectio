eb4b3436698ad0f148c23b5f68e4566a7d2e580f
jona799t <jonathan.b.skettrup@gmail.com>
2022-12-18 14:33:55 +0100

    Fjernede | fra aktuelt

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 from pathlib import Path
 
-VERSION = '1.8.3'
+VERSION = '1.8.4'
 DESCRIPTION = 'En SDK til gymnasie siden Lectio'
 long_description = (Path(__file__).parent / "README.md").read_text()
 
