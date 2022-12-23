c6f1f49f97426d27ec62f9334bf023ac93287aec
jona799t <jonathan.b.skettrup@gmail.com>
2022-12-23 14:48:57 +0100

    Nu med error code

--- lectio/_refreshCookie.py
+++ lectio/_refreshCookie.py
@@ -2,17 +2,17 @@ from .imports import *
 
 def refreshCookie(self):
     success = False
+    error = None
     try:
         headers = {
             "cookie": f"lectiogsc={self.session.cookies['lectiogsc']}; autologinkey={self.session.cookies['autologinkey']}",
         }
         resp = requests.get(f"https://www.lectio.dk/lectio/{self.skoleId}/forside.aspx", headers=headers, allow_redirects=False)
         newSessionId = resp.cookies["ASP.NET_SessionId"]
-        print(newSessionId)
         self.session.cookies.set('ASP.NET_SessionId', newSessionId, domain='lectio.dk', path='/')
 
         success = True
-    except Exception:
-        pass
+    except Exception as e:
+        error = e
 
-    return {"success": success}
+    return {"success": success, "error": error}
